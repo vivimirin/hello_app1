bb9bb39a170e2761b5ef5a7b8bbf2dc3f5f540e6
vivimirin <114142522+vivimirin@users.noreply.github.com>
2023-12-24 13:48:22 +0000

    improve version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def hello
+    render html: "hola,mundo!"
+  end
+end
+class ApplicationController < ActionController::Base
+
+  def goodbye
+    render html: "goodbye, world!"
+  end
 end
